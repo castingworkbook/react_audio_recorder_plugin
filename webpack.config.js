0db75b09b1d120ae4e4b6ef0cd4766555f8d8524
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2019-12-07 18:56:03 +0000

    changed style dir folder

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   rules: [
     {
       test: /\.css$/,
-      include: path.join(__dirname, 'src/components'),
+      include: path.join(__dirname, 'src/style'),
       use: [
         'style-loader',
         {
