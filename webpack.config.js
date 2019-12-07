16117e5a89a6315cb463b6adc9b0e32037617b0f
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2019-12-07 18:51:00 +0000

    changed webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,21 @@ module.exports = {
   externals: {
     react: 'React',
     'react-dom': 'ReactDOM'
-  }
+  },
+  rules: [
+    {
+      test: /\.css$/,
+      include: path.join(__dirname, 'src/components'),
+      use: [
+        'style-loader',
+        {
+          loader: 'typings-for-css-modules-loader',
+          options: {
+            modules: true,
+            namedExport: true
+          }
+        }
+      ]
+    }
+  ]
 };
